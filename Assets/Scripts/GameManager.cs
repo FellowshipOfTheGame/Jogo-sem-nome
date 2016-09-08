7074827c064c6dad02dc960070ad45cfb1f7b404
lucas1131 <lucassoares1793@gmail.com>
2016-09-08 15:50:38 -0300

    Resize de elementos da GUI, botões e players de placehold, GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public Player p1, p2;
+    public Timer roundCounter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    private float time = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        time -= Time.deltaTime;
+        if(time <= 0){
+            time = 0;
+            print("Timer zerou");
+        }
+    }
+
+    void OnGUI() {
+        GUI.Box(new Rect(10, 10, 50, 20), "Test: " + time.ToString("0"));
+    }
+}
