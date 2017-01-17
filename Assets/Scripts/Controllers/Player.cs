f5de6c126ea93ceeeb912151b04c92cdd4d50f3d
Lucas Turci <lucas.turci@gmail.com>
2017-01-17 17:31:46 -0200

    Adicionados comentarios a Player.cs e algumas correcoes

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,51 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-enum Action : byte {NOOP, ATK, DEF, REL}
+public enum Action : byte {NOOP, ATK, DEF, REL}
 
 // Responsavel por ter as informacoes do player e enviar as acoes realizadas para o GameManager
 public class Player : MonoBehaviour {
 
-	private int defCount, defMax;
-	private int ammo, maxBullets;
+	private int defCount, defMax; 
+	private int ammo, maxBullets; 
 	private Action action;
 
 	// Use this for initialization
 	void Start () {
 		ammo = 0;
 		defCount = 0;
+		defMax = 5; 	//placeholder
+		maxBullets = 5; //placeholder
 	}
 
-	/* Só seleciona a ação */
+	/* Setter da ação */
 	public void SetAction(Action act) {
 		action = act;
 	}
 
 	/* Realiza a ação, e será chamada quando o timer acabar */
-	public void DoAction() {
+	public Action DoAction() {
 		switch(action) {
-			case DEF:
-				defCount++;
-				if(defCount > defMax) {//nao pode defender mais
-					ammo = maxBullets; 
+			case DEF: // player quer defender
+				defCount++; //incrementa a contagem de defesas
+				if(defCount > defMax) { //nao pode defender mais
+					defCount = defMax; 
 					return NODEF;
 				}
+				
 				return CANDEF;
-			case ATK:
-				defCount = 0;
-				if(ammo == 0) return NOAMMO;
-				ammo--;
-				return CANATK;
-			case REL:
-				defCount = 0;
+			case ATK: // player quer atacar
+				defCount = 0; //reinicia a contagem de defesas
+				if(ammo == 0) return NOAMMO; //nao pode atacar				
+				ammo--; 
+
+				return CANATK;		
+			case REL: // player quer recarregar
+				defCount = 0; //reinicia a contagem de defesas
 				ammo++;
-				if(ammo > maxBullets) {//nao pode carregar mais
+				if(ammo > maxBullets) { //nao pode carregar mais
 					ammo = maxBullets; 
 					return NOREL;
 				}
+
 				return CANREL;
-			default: 
-				defCount = 0;
+			default: // player nao quer fazer nada
+				defCount = 0; //reinicia a contagem de defesas
 				return NOTHING;
 		}
 	}
