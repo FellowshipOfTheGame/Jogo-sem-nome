f3eb3e4c28c1c308ff05d21ba91cb5574e9d38cd
kori4r2 <xitsilvax@hotmail.com>
2017-01-17 11:00:48 -0300

    added comments and getter to timer, unnecessary scripts deleted

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -12,6 +12,7 @@ public class Timer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        time = 0;
         counting = false;
 	}
 	
@@ -21,22 +22,35 @@ public class Timer : MonoBehaviour {
 		    UpdateTimer();
 	}
 
+    // getter da variável time
+    public float getTime() {
+        return time;
+    }
+
+    // Essa função deve ser chamada de fora para iniciar o timer
     public void StartTimer(float maxTime, VoidFunction function){
+        // O tempo máximo e o tempo atual são atualizados
         this.maxTime = maxTime;
         time = maxTime;
+        // A função a ser executada no final da contagem é armazenada
         timerFunction = function;
+        // Indica o início da contagem
         counting = true;
     }
 
     void UpdateTimer(){
+        // Decrementa o tempo atual
         time -= Time.deltaTime;
+        // Quando a contagem acabar
         if(time <= 0){
+            // Indica o fim, zera time e chama a função salva
             counting = false;
             time = 0;
             timerFunction();
         }
     }
 
+    // Mostra o valor da contagem atual
 	void OnGUI() {
 		GUI.Box(new Rect(15, 15, 55, 20), "Test: " + time.ToString("0"));
 	}
